843385c4af64d36239c1cacb3ecfe3d5c9241a59
Julien Kauffmann <julien.kauffmann@freelan.org>
2017-04-25 23:49:19 -0400

    Added code to register third-party resource

--- cmd/krc_operator/main.go
+++ cmd/krc_operator/main.go
@@ -2,23 +2,46 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
+	"github.com/ereOn/k8s-redis-cluster-operator/pkg/k8s"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func main() {
-	kubeconfig := flag.String("kubeconfig", filepath.Join(os.Getenv("HOME"), ".kube", "config"), "absolute path to the kubeconfig file")
+var kubeconfig = flag.String("kubeconfig", filepath.Join(os.Getenv("HOME"), ".kube", "config"), "absolute path to the kubeconfig file")
 
+func main() {
 	flag.Parse()
 
+	config := getConfig()
+	clientset := getClientset(config)
+
+	k8s.RegisterThirdPartyResource(clientset)
+	defer k8s.UnregisterThirdPartyResource(clientset)
+	time.Sleep(time.Second * 10)
+}
+
+func getConfig() *rest.Config {
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(config)
+	return config
+}
+
+func getClientset(config *rest.Config) *kubernetes.Clientset {
+	clientset, err := kubernetes.NewForConfig(config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return clientset
 }
--- pkg/k8s/third_party.go
+++ pkg/k8s/third_party.go
@@ -1,7 +1,77 @@
 package k8s
 
-import "k8s.io/client-go/kubernetes"
+import (
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
+)
 
-func GetThirdPartyResource() {
-	kubernetes.Foo()
+// K8SRedisClusterResourceName is the "Redis Cluster" resource name.
+var K8SRedisClusterResourceName = "k8s-redis-cluster.freelan.org"
+
+// RegisterThirdPartyResource makes sure the third-party resource is registered.
+func RegisterThirdPartyResource(clientset *kubernetes.Clientset) (*v1beta1.ThirdPartyResource, error) {
+	tpr, err := GetThirdPartyResource(clientset)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if tpr != nil {
+		return tpr, nil
+	}
+
+	return CreateThirdPartyResource(clientset)
+}
+
+// UnregisterThirdPartyResource makes sure the third-party resource is unregistered.
+func UnregisterThirdPartyResource(clientset *kubernetes.Clientset) error {
+	tpr, err := GetThirdPartyResource(clientset)
+
+	if err != nil {
+		return err
+	}
+
+	if tpr != nil {
+		return DeleteThirdPartyResource(clientset)
+	}
+
+	return nil
+}
+
+// GetThirdPartyResource returns the third-party resource if it was registered
+// or nil otherwise.
+func GetThirdPartyResource(clientset *kubernetes.Clientset) (*v1beta1.ThirdPartyResource, error) {
+	tpr, err := clientset.ExtensionsV1beta1().ThirdPartyResources().Get(K8SRedisClusterResourceName, metav1.GetOptions{})
+
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	return tpr, nil
+}
+
+// CreateThirdPartyResource creates the third-party resource.
+func CreateThirdPartyResource(clientset *kubernetes.Clientset) (*v1beta1.ThirdPartyResource, error) {
+	tpr := &v1beta1.ThirdPartyResource{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: K8SRedisClusterResourceName,
+		},
+		Versions: []v1beta1.APIVersion{
+			{Name: "v1"},
+		},
+		Description: "A Redis Cluster",
+	}
+
+	return clientset.ExtensionsV1beta1().ThirdPartyResources().Create(tpr)
+}
+
+// DeleteThirdPartyResource deletes the third-party resource.
+func DeleteThirdPartyResource(clientset *kubernetes.Clientset) error {
+	return clientset.ExtensionsV1beta1().ThirdPartyResources().Delete(K8SRedisClusterResourceName, &metav1.DeleteOptions{})
 }
